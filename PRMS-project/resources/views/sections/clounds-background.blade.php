a9b55b3f3db5f7f5f847adf8c38dc563209b2c8d
KIMGITO <kimanthidennis02@gmail.com>
2024-04-07 16:52:27 +0300

    one

--- PRMS-project/resources/views/sections/clounds-background.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- clound  background JS--}}
-    
-<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vanta/0.5.21/vanta.clouds.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-{{-- Calendar --}}
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js"></script>
-
